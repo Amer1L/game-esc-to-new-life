e6ef139e1d2089ace3758a4289ca3c688c4530d8
Amer1L <119682709+Amer1L@users.noreply.github.com>
2024-10-30 19:58:04 +0200

    Update 8(PostProcessing)
    Благодаря постпроцессингу игра стала выглядеть гораздо лучше. Ещё сделал вместо звёзд на спрайте фона они теперь партиклы. Также немного улучшил механику захода в здание на планете 2.

--- Escape to a new life/Assets/Scripts/HouseInsade.cs
+++ Escape to a new life/Assets/Scripts/HouseInsade.cs
@@ -5,19 +5,16 @@ using UnityEngine;
 public class HouseInsade : MonoBehaviour
 {
 
-    [SerializeField] private GameObject _outsideLight;
     [SerializeField] private GameObject _houseOutSprite;
     [SerializeField] private GameObject _insideObject;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        _outsideLight.SetActive(false);
         _houseOutSprite.SetActive(false);
         _insideObject.SetActive(true);
     }
     private void OnTriggerExit2D(Collider2D collision)
     {
-        _outsideLight.SetActive(true);
         _houseOutSprite.SetActive(true);
         _insideObject.SetActive(false);
     }
